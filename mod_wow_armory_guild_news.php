7badb13fb8cdae98f2e3b3ed59d1cd3b9cbdf23f
b2un0 <branko.wilhelm@gmail.com>
2013-11-27 20:20:03 +0100

    enh: language files

--- mod_wow_armory_guild_news.php
+++ mod_wow_armory_guild_news.php
@@ -6,7 +6,7 @@
  * @copyright  (c) 2013 Branko Wilhelm
  * @license    GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
  */
- 
+
 defined('_JEXEC') or die;
 
 require_once dirname(__FILE__) . '/helper.php';
@@ -15,9 +15,9 @@ $news = new mod_wow_armory_guild_news($params);
 
 $news = $news->data();
 
-if(!is_array($news)) {
-	echo $news;
-	return;
+if (!is_array($news)) {
+    echo $news;
+    return;
 }
 
 require JModuleHelper::getLayoutPath($module->module, $params->get('layout', 'default'));
